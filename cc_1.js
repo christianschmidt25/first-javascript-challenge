7a5827bbfbc2e0f0ea95db61f6a87594f67cf3cc
Christian Schmidt <cjschmidt33@gmail.com>
2024-08-29 16:07:06 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,15 @@ var isActive = true;
 console.log(customerName, typeof(customerName));
 console.log(customerID, typeof(customerID));
 console.log(isActive, typeof(isActive));
+
+
+
+// Task 2: Compound Data Types
+
+let products = ["Basketball", "Sneakers", "Jerseys"];
+const productDetails = {
+    name: "Wilson",
+    price: 59.00,
+    inStock: true,
+}
+console.log(products, productDetails);
