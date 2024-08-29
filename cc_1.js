b2d14bb1c3a59bf354b8e0fd39565d5a6011babf
Christian Schmidt <cjschmidt33@gmail.com>
2024-08-29 15:52:18 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let customerName = "Cade Cunningham";
+const customerID = "2";
+var isActive = true;
+
+console.log(customerName, typeof(customerName));
+console.log(customerID, typeof(customerID));
+console.log(isActive, typeof(isActive));
